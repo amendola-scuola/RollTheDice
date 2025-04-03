ac341feb6fc83de8902f2db649df4654d84d546a
Francesco Amendola <francesco.amendola@risikamente.it>
2025-04-04 00:40:40 +0200

    aggiunti setImageResource e SecondActivity

--- app/src/main/java/com/example/rollthedice5bi/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice5bi/MainActivity.kt
@@ -1,27 +1,68 @@
 package com.example.rollthedice5bi
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceFaces: HashMap<Int, Int>
+    lateinit var imageViewDice: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         var btnRoll = findViewById<Button>(R.id.buttonRoll)
+
         btnRoll.setOnClickListener(View.OnClickListener {
-            var mioRandom = (1..6).random()
-            var mioToast = Toast.makeText(this,
-                "NUMERO ESTRATTO: $mioRandom",
-                Toast.LENGTH_SHORT)
-            mioToast.show()
+            messaggioToast("DADI LANCIATI!")
+
+            var mioRandom = estraiNumero()
+            messaggioToast("ESTRATTO NUMERO $mioRandom!")
+
+            loadFaces()
+            setImage(mioRandom)
+            lanciaIntent(mioRandom)
         })
+    }
+
+    private fun messaggioToast(msg: String) {
+        var mioToast = Toast.makeText(
+            this,
+            msg,
+            Toast.LENGTH_SHORT
+        )
+        mioToast.show()
+    }
+
+    private fun loadFaces() {
+        diceFaces = HashMap()
+        for (i in 1..6) {
+            diceFaces.put(i, resources.getIdentifier("dice_face_$i", "drawable", packageName))
+        }
+    }
+
+    private fun estraiNumero(): Int {
+        return (1..6).random()
+    }
+
+    private fun lanciaIntent(mioRandom: Int) {
+        var mioIntent = Intent(this, SecondActivity::class.java)
+        mioIntent.putExtra("NUMERO", mioRandom)
+        startActivity(mioIntent)
+    }
+
+    private fun setImage(mioRandom: Int) {
+        imageViewDice = findViewById(R.id.imageViewDice)
+        imageViewDice.setImageResource(diceFaces.get(mioRandom)!!)
     }
 }
