b69f010bf8b2be7b3a0d9735b4d38127ad85057f
Amendola <francesco.amendola@risikamente.it>
2025-04-08 12:45:58 +0200

    Import ottimizzati

--- app/src/main/java/com/example/rollthedice5bi/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice5bi/MainActivity.kt
@@ -8,9 +8,6 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceFaces: HashMap<Int, Int>
